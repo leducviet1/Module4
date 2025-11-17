107dc09bb0abe556cb22e257f142e21e76297e15
LeDucViet <ldviettb04@gmail.com>
2025-11-17 10:52:15 +0700

    test done

--- CaseStudy/src/main/java/com/example/casestudy/controller/ProductController.java
+++ CaseStudy/src/main/java/com/example/casestudy/controller/ProductController.java
@@ -7,7 +7,6 @@ import com.example.casestudy.service.StorageService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
